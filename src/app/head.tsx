c88ed51a6b625f79728720190ef223b07a87bc53
Henrique Garcia <henriquegarcia.dev@gmail.com>
2023-06-02 14:13:58 -0300

    Save config

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-      <link rel="icon" href="/favicon.ico" />
+      {/* <link rel="icon" href="/favicon.ico" /> */}
 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
